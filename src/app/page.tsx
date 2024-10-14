1f5229d1e286283a2af6a9d359273f399a191004
y-shoraka <shorakayasaman@gmail.com>
2024-10-14 15:40:15 +0330

    delete the route for next auth to deploy the project first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,26 @@
-import { signIn, signOut, useSession } from 'next-auth/react';
+'use client'
+
+// import { signIn, signOut, useSession } from 'next-auth/react';
 import React from 'react'
 
 
 function HomePage() {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
   return (
-    <div>
-    {!session ? (
-      <>
-        <p>You are not signed in.</p>
-        <button onClick={() => signIn('google')}>Sign in with Google</button>
-      </>
-    ) : (
-      <>
-        <p>Welcome, {session?.user?.name??''}</p>
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )}
-  </div>
+  //   <div>
+  //   {!session ? (
+  //     <>
+  //       <p>You are not signed in.</p>
+  //       <button onClick={() => signIn('google')}>Sign in with Google</button>
+  //     </>
+  //   ) : (
+  //     <>
+  //       <p>Welcome, {session?.user?.name??''}</p>
+  //       <button onClick={() => signOut()}>Sign out</button>
+  //     </>
+  //   )}
+  // </div>
+  <div>hello it is homepage</div>
   )
 }
 
